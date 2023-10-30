7c4a46897f7c9a90c90e56102de564ec295de1c1
Mustafa ASGHARI <mustafaasghari@Mustafas-MacBook-Air.local>
2023-10-29 19:03:16 -0700

    add update style

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -2,41 +2,63 @@ import React from "react";
 import upload from "../../assets/images/Upload-video-preview.jpg";
 import publish from "../../assets/images/Icons/publish.svg";
 import { useNavigate } from "react-router-dom";
+import "./Upload.scss";
 
 function Upload(props) {
   const navigate = useNavigate();
   return (
     <>
-      <h1>Upload Video</h1>
-      <div>
-        <label>VIDEO THUMBNAIL</label>
-        <img src={upload} alt="upload" />
-      </div>
-      <div>
-        <label>TITLE YOUR VIDEO</label>
-        <input type="text" placeholder="Add a title to your video" />
-      </div>
-      <div>
-        <label>ADD A VIDEO DESCRIPTION</label>
-        <input type="text" placeholder="Add a description to your video" />
-      </div>
+      <div className="upload">
+        <div className="upload__head">
+          <h1>Upload Video</h1>
+        </div>
 
-      <button
-        onClick={() => {
-          alert("Successfully Published");
-          navigate("/");
-        }}
-      >
-        <img src={publish} alt="publish" />
-        PUBLISH
-      </button>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        CANCEL
-      </button>
+        <div className="upload__container">
+          <div className="upload__input">
+            <label className="upload__input-label">VIDEO THUMBNAIL</label>
+            <img className="upload__input-img" src={upload} alt="upload" />
+          </div>
+          <div className="upload__input-maintop">
+            <label className="upload__input-label">TITLE YOUR VIDEO</label>
+            <input
+              className="upload__input-text"
+              type="text"
+              placeholder="Add a title to your video"
+            />
+          </div>
+          <div className="upload__input-mainbottom">
+            <label className="upload__input-label">
+              ADD A VIDEO DESCRIPTION
+            </label>
+            <textarea
+              className="upload__input-textarea"
+              rows="5"
+              placeholder="Add a description to your video"
+            />
+          </div>
+        </div>
+
+        <div className="button">
+          <button
+            className="button__publish"
+            onClick={() => {
+              alert("Successfully Published");
+              navigate("/");
+            }}
+          >
+            <img src={publish} alt="publish" />
+            <span>PUBLISH</span>
+          </button>
+          <button
+            className="button__cancel"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <span>CANCEL</span>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
